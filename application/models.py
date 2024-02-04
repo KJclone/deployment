ed4afa5f17f078127e072d2786b1bb0715eb2323
KJclone <158156169+KJclone@users.noreply.github.com>
2024-02-04 11:01:21 +0530

    Commit 4 with Object naming feature

--- application/models.py
+++ application/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class database(models.Model):
     Name=models.CharField(max_length=100)
     Number=models.CharField(max_length=100)
+    def __str__(self):
+        return self.Name
